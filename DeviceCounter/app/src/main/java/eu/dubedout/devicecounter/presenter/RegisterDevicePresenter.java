2b6b12108d6603a5caf3164d237e78734e654933
Vincent Dubedout <vincent.dubedout@pj.ca>
2015-10-27 16:16:45 -0400

    add label model and button click

---
+++ DeviceCounter/app/src/main/java/eu/dubedout/devicecounter/presenter/RegisterDevicePresenter.java
@@ -0,0 +1,20 @@
+package eu.dubedout.devicecounter.presenter;
+
+import android.os.Bundle;
+import android.text.Editable;
+
+public class RegisterDevicePresenter {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO: VincentD 15-10-27 save instance state, recover on rotation
+    }
+
+    public void sendButtonClick(Editable deviceLabelText, Editable deviceModelText) {
+        // get all data
+        // create Device Object
+        // transform using parse
+        // send to parse server
+        // update local
+    }
+}
